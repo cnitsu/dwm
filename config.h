1f11db981f92b75e38c084e9ae167032841c3f23
itsu <hychun1314@163.com>
2025-10-30 06:19:16 +0800

    feat: set the fontsize to default

--- config.h
+++ config.h
@@ -8,8 +8,8 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
-static const char *fonts[]          = { "Maple Mono NF CN:style=Regular:size=26" };
-static const char dmenufont[]       = "Maple Mono NF CN:style=Regular:size=26";
+static const char *fonts[]          = { "Maple Mono NF CN:style=Regular:size=10" };
+static const char dmenufont[]       = "Maple Mono NF CN:style=Regular:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
